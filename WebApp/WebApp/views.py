e7161f132a8cdc25503eaa2d53ed8ac1271f8954
Eric Majidian <ericmajidian@gmail.com>
2020-03-24 17:04:06 -0700

    CSRF verfication fix?

--- WebApp/WebApp/views.py
+++ WebApp/WebApp/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User, Group
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse 
 from rest_framework import viewsets
 from rest_framework import permissions
@@ -15,15 +16,16 @@ class StationViewSet (viewsets.ModelViewSet):
     serializer_class = StationSerializer
 
 
+@csrf_exempt
 def LockStationView (request):
     if request.method == 'POST':
-        payload = json.loads (requests.body)
-        staiton_uuid = payload['uuid']
+        payload = json.loads (request.body)
+        station_uuid = payload['uuid']
         lock_id = payload['lock_id']
 
         station = Station.objects.get (uuid = station_uuid)
 
-        r = requests.get (station.ip + 'api path stuff (lock_id)')
+        r = requests.get (station.ip + ':8000/lock/' + lock_id)
         return HttpResponse ("Locked *thumbs up*")
     else:
         return HttpResponse ("Miss me with that GET")
