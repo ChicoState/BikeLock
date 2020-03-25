055b8d1922c89e418443f5ce2cfa2a840d418c99
carlonavone <cnavone@mail.csuchico.edu>
2020-03-25 16:08:42 -0700

    - Added in second drop down for lock - General styling changes

--- Mobile/lib/main.dart
+++ Mobile/lib/main.dart
@@ -37,17 +37,21 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String _counter = "false";
 
-  String nameCity = "";
+  var _AvailableStations = ['Station 1', 'Station 2'];
+  var _currentStation = 'Station 1';
+
   var _AvailableLocks = ['Lock 1', 'Lock 2'];
-  var _currentItemSelected = 'Lock 1';
+  var _currentLock= 'Lock 1';
+
+
 
   void lock_and_unlock() {
 
     //TODO Create drop down menu with list of the locks, hardcode for now
     //Variables for interacting with the locks, set them as you need to
-    String UUID;
-    int lock_ID;
-    int state;
+    String UUID = "UUID Placeholder";
+    int lock_ID = 00000;
+    int state = 00000;
 
     //Creating HTTP Object
     var HTTP = HTTPHelper(UUID, lock_ID, state);
@@ -93,42 +97,78 @@ class _MyHomePageState extends State<MyHomePage> {
         // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+
           children: <Widget>[
-          Text('Your Bike is Locked up:',),
-          Text('$_counter',),
-          DropdownButton<String>(
-          items: _AvailableLocks.map((String dropDownStringItem) {
-            return DropdownMenuItem<String>(
-              value: dropDownStringItem,
-              child: Text(dropDownStringItem),
-            );
-          }).toList(),
+            Text('Your Bike is Locked up:',  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),  ),
+            Text('$_counter',  style: TextStyle( fontSize: 20, )),
+            SizedBox(height: 50),
+            Text('Station ID:',  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+            DropdownButton<String>(
+              items: _AvailableStations.map((String dropDownStringItem) {
+                return DropdownMenuItem<String>(
+                  value: dropDownStringItem,
+                  child: Text(dropDownStringItem),
+                );
+              }).toList(),
 
-          onChanged: (String newValueSelected) {
-            // Your code to execute, when a menu item is selected from drop down
-            _onDropDownItemSelected(newValueSelected);
-          },
+              onChanged: (String newValueSelected) {
+                // Your code to execute, when a menu item is selected from drop down
+                _onStationItemSelected(newValueSelected);
+              },
 
-          value: _currentItemSelected,
+              value: _currentStation,
+            ),
+            SizedBox(height: 50),
+            Text('Lock ID:',  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+            DropdownButton<String>(
+              items: _AvailableLocks.map((String dropDownStringItem) {
+                return DropdownMenuItem<String>(
+                  value: dropDownStringItem,
+                  child: Text(dropDownStringItem, ),
+                );
+              }).toList(),
 
+              onChanged: (String newValueSelected) {
+                // Your code to execute, when a menu item is selected from drop down
+                _onLockItemSelected(newValueSelected);
+              },
+
+              value: _currentLock,
+
+            ),
+          ],
         ),
-        ],
-      ),
 
-    ),
-    floatingActionButton: FloatingActionButton(
-    onPressed: lock_and_unlock,
-    tooltip: 'Increment',
-    //TODO Alternating lock & unlock icon
-    child: (_counter == "true") ? Icon(Icons.lock) : Icon(Icons.lock_open),
-    backgroundColor: (_counter == "true") ? Colors.red : Colors.green,
-    ),
-        // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      floatingActionButton: Container(
+        height: 100.0,
+        width: 100.0,
+
+        child: FittedBox(
+          child: FloatingActionButton(
+            onPressed: lock_and_unlock,
+            tooltip: 'Increment',
+            //Alternating Icon
+            child: (_counter == "true") ? Icon(Icons.lock) : Icon(Icons.lock_open),
+            //Alternating Icon Color
+            backgroundColor: (_counter == "true") ? Colors.red : Colors.green,
+            splashColor: Colors.amber,
+          ),
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-  void _onDropDownItemSelected(String newValueSelected) {
+
+  void _onStationItemSelected(String newValueSelected) {
     setState(() {
-      this._currentItemSelected = newValueSelected;
+      this._currentStation = newValueSelected;
+    });
+  }
+
+  void _onLockItemSelected(String newValueSelected) {
+    setState(() {
+      this._currentLock = newValueSelected;
     });
   }
 }
