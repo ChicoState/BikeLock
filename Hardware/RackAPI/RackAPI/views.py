13f53e16d3dc70ddcf22d967e926a67402e89e4b
lvanderspek <lvanderspek@mail.csuchico.edu>
2020-03-26 04:03:58 +0000

    added int casting for lockid and state fields

--- Hardware/RackAPI/RackAPI/views.py
+++ Hardware/RackAPI/RackAPI/views.py
@@ -7,15 +7,15 @@ import json
 def setLock(request):
     if request.method == 'POST':
         payload = json.loads(request.body)
-        state = payload['state']
-        lockid = payload['lock_id']
+        state = int(payload['state'])
+        lockid = int(payload['lock_id'])
 
         lock.set_Lock_State(lockid,state)
         return JsonResponse(payload)
     
     if request.method == 'GET':
         payload = json.loads(request.body)
-        lockid = payload['lock_id']
+        lockid = int(payload['lock_id'])
         payload['state'] = lock.get_Lock_State(lockid)
 
         return JsonResponse(payload)
