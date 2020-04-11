8356bdb96e915e7f4a2a5cfa514e54e11e99f497
lvanderspek <lvanderspek@mail.csuchico.edu>
2020-04-11 21:18:05 +0100

    extended get_Lock_State() to get states of all locks by default

--- Hardware/Lock/lock.py
+++ Hardware/Lock/lock.py
@@ -8,11 +8,16 @@ def init():
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(pin, GPIO.OUT)
 
+def get_Lock_Count():
+    return len(ledpins)
+
 def set_Lock_State(lockid, state):
     GPIO.output(ledpins[lockid], state)
 
-def get_Lock_State(lockid):
+def get_Lock_State(lockid = -1):
+    if lockid == -1:
+        states = []
+        for pin in range(get_Lock_Count()):
+            states.append(get_Lock_State(pin))
+        return states
     return GPIO.input(ledpins[lockid])
-
-def get_Lock_count():
-    return len(ledpins)
