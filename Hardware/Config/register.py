e4fd498e1e49ae663de72e86f867f6e7c26a5520
lvanderspek <lvanderspek@mail.csuchico.edu>
2020-04-14 19:42:53 +0100

    handle inactive server by retrying awakening message

--- Hardware/Config/register.py
+++ Hardware/Config/register.py
@@ -1,6 +1,17 @@
 import requests
+import time
+
+LOGIN_URL = 'http://rackServer:8000/api/stations/'
 
 def registerRack(uuid):
-    LOGIN_URL = 'http://rackServer:8000/api/stations/'
-    r = requests.post(LOGIN_URL, json={'uuid':uuid})
+    while True:
+        time.sleep(5)
+
+        try:
+            r = requests.post(LOGIN_URL, json={'uuid':uuid})
+        except requests.exceptions.ConnectionError:
+            continue
+
+        if r.status_code == 200:
+            return
 
